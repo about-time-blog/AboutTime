de53672ce594960f471c6635741a1c9fa12a240d
Mikkel Riise Lund <mrl@cvation.com>
2019-09-11 19:14:39 +0200

    Adds dummy object for easy testing.

---
+++ src/AboutTime.Dummy/NodaTimeDataStructure.cs
@@ -0,0 +1,92 @@
+﻿using NodaTime;
+
+namespace AboutTime.Dummy
+{
+    public class NodaTimeDataStructure
+    {
+        public NodaTimeDataStructure()
+        {
+            IClock clock = SystemClock.Instance;
+            DateTimeZone = DateTimeZoneProviders.Tzdb["Europe/Copenhagen"];
+            Instant = clock.GetCurrentInstant();
+
+            ZonedDateTime = Instant.InZone(DateTimeZone);
+            OffsetDateTime = ZonedDateTime.ToOffsetDateTime();
+            OffsetDate = OffsetDateTime.ToOffsetDate();
+            OffsetTime = OffsetDateTime.ToOffsetTime();
+            Offset = OffsetDateTime.Offset;
+            LocalDateTime = ZonedDateTime.LocalDateTime;
+            LocalDate = LocalDateTime.Date;
+            LocalTime = LocalDateTime.TimeOfDay;
+            IsoDayOfWeek = LocalDateTime.DayOfWeek;
+            Duration = Duration.FromDays(2) + Duration.FromHours(14) + Duration.FromMinutes(39) + Duration.FromSeconds(17);
+
+            var periodBuilder = new PeriodBuilder
+            {
+                Months = 2,
+                Days = 1,
+                Hours = 4,
+                Minutes = 5
+            };
+            Period = periodBuilder.Build();
+
+            Interval = new Interval(Instant, Instant + Duration);
+
+            DateInterval = new DateInterval(LocalDate, LocalDate + Period.FromDays(5));
+        }
+
+        public DateInterval DateInterval { get; set; }
+        public DateTimeZone DateTimeZone { get; set; }
+        public Duration Duration { get; set; }
+        public Instant Instant { get; set; }
+        public Interval Interval { get; set; }
+        public IsoDayOfWeek IsoDayOfWeek { get; set; }
+        public LocalDate LocalDate { get; set; }
+        public LocalDateTime LocalDateTime { get; set; }
+        public LocalTime LocalTime { get; set; }
+        public Offset Offset { get; set; }
+        public OffsetDate OffsetDate { get; set; }
+        public OffsetDateTime OffsetDateTime { get; set; }
+        public OffsetTime OffsetTime { get; set; }
+        public Period Period { get; set; }
+        public ZonedDateTime ZonedDateTime { get; set; }
+
+        #region Nullable With Values
+
+        public Duration? NullableDuration => Duration;
+        public Instant? NullableInstant => Instant;
+        public Interval? NullableInterval => Interval;
+        public IsoDayOfWeek? NullableIsoDayOfWeek => IsoDayOfWeek;
+        public LocalDate? NullableLocalDate => LocalDate;
+        public LocalDateTime? NullableLocalDateTime => LocalDateTime;
+        public LocalTime? NullableLocalTime => LocalTime;
+        public Offset? NullableOffset => Offset;
+        public OffsetDate? NullableOffsetDate => OffsetDate;
+        public OffsetDateTime? NullableOffsetDateTime => OffsetDateTime;
+        public OffsetTime? NullableOffsetTime => OffsetTime;
+        public ZonedDateTime? NullableZonedDateTime => ZonedDateTime;
+
+        #endregion
+
+        #region Nullable With Default Values
+
+        public DateInterval NullDateInterval => default;
+        public DateTimeZone NullDateTimeZone => default;
+        public Duration? NullDuration => default;
+        public Instant? NullInstant => default;
+        public Interval? NullInterval => default;
+        public IsoDayOfWeek? NullIsoDayOfWeek => default;
+        public LocalDate? NullLocalDate => default;
+        public LocalDateTime? NullLocalDateTime => default;
+        public LocalTime? NullLocalTime => default;
+        public Offset? NullOffset => default;
+        public OffsetDate? NullOffsetDate => default;
+        public OffsetDateTime? NullOffsetDateTime => default;
+        public OffsetTime? NullOffsetTime => default;
+        public Period NullPeriod => default;
+        public PeriodBuilder NullPeriodBuilder => default;
+        public ZonedDateTime? NullZonedDateTime => default;
+
+        #endregion
+    }
+}
---
+++ src/AboutTime.Test/Dummy.NodaTimeDataStructureTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using AboutTime.Dummy;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using NodaTime;
+using Xunit;
+
+namespace AboutTime.Test
+{
+    public class NodaTimeDataStructureTests
+    {
+        [Fact]
+        public void Dummy()
+        {
+            // Arrange
+            var dummy = new NodaTimeDataStructure();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                ((IEquatable<DateInterval>) dummy.DateInterval).Should().NotBe(default(DateInterval));
+                dummy.DateTimeZone.Should().NotBe(default(DateTimeZone));
+                dummy.Duration.Should().NotBe(default(Duration));
+                dummy.Instant.Should().NotBe(default(Instant));
+                dummy.Interval.Should().NotBe(default(Interval));
+                dummy.IsoDayOfWeek.Should().NotBe(default(IsoDayOfWeek));
+                dummy.LocalDate.Should().NotBe(default(LocalDate));
+                dummy.LocalDateTime.Should().NotBe(default(LocalDateTime));
+                dummy.LocalTime.Should().NotBe(default(LocalTime));
+                dummy.Offset.Should().NotBe(default(Offset));
+                dummy.OffsetDate.Should().NotBe(default(OffsetDate));
+                dummy.OffsetDateTime.Should().NotBe(default(OffsetDateTime));
+                dummy.OffsetTime.Should().NotBe(default(OffsetTime));
+                dummy.Period.Should().NotBe(default(Period));
+                dummy.ZonedDateTime.Should().NotBe(default(ZonedDateTime));
+
+                dummy.NullableDuration.Should().NotBe(default(Duration));
+                dummy.NullableInstant.Should().NotBe(default(Instant));
+                dummy.NullableInterval.Should().NotBe(default(Interval));
+                dummy.NullableIsoDayOfWeek.Should().NotBe(default(IsoDayOfWeek));
+                dummy.NullableLocalDate.Should().NotBe(default(LocalDate));
+                dummy.NullableLocalDateTime.Should().NotBe(default(LocalDateTime));
+                dummy.NullableLocalTime.Should().NotBe(default(LocalTime));
+                dummy.NullableOffset.Should().NotBe(default(Offset));
+                dummy.NullableOffsetDate.Should().NotBe(default(OffsetDate));
+                dummy.NullableOffsetDateTime.Should().NotBe(default(OffsetDateTime));
+                dummy.NullableOffsetTime.Should().NotBe(default(OffsetTime));
+                dummy.NullableZonedDateTime.Should().NotBe(default(ZonedDateTime));
+
+                ((IEquatable<DateInterval>)dummy.NullDateInterval).Should().BeNull();
+                dummy.NullDateTimeZone.Should().BeNull();
+                dummy.NullDuration.Should().BeNull();
+                dummy.NullInstant.Should().BeNull();
+                dummy.NullInterval.Should().BeNull();
+                dummy.NullIsoDayOfWeek.Should().BeNull();
+                dummy.NullLocalDate.Should().BeNull();
+                dummy.NullLocalDateTime.Should().BeNull();
+                dummy.NullLocalTime.Should().BeNull();
+                dummy.NullOffset.Should().BeNull();
+                dummy.NullOffsetDate.Should().BeNull();
+                dummy.NullOffsetDateTime.Should().BeNull();
+                dummy.NullOffsetTime.Should().BeNull();
+                dummy.NullPeriod.Should().BeNull();
+                dummy.NullZonedDateTime.Should().BeNull();
+            }
+        }
+    }
+}
